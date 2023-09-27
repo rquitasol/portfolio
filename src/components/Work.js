e357df314c5cfa28c6a4a6993e4e2cccbb87e06a
rquitasol <115167445+rquitasol@users.noreply.github.com>
2023-09-28 03:04:10 +0800

    Added style for work page
    Added carousel for work page. Added initial design for work card

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,31 +1,79 @@
 import React from "react";
 import temp from "../assets/temp.jpg";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "../style/Carousel.css";
+const Work = ({ isDesktop }) => {
+  const workData = [
+    {
+      company: "iScale Solution",
+      position: "Software Engineer",
+      project: "Salesforce",
+      info: 'Create major features requested by the Product Manager to be presented to the client.Demo of newly created feature to Product Manager to getfeedback on the requested feature.Fix bugs reported by teamCode review done by other teammatesCreate automation test using Selenium"',
+    },
 
-const Work = () => {
-  const workCard = (
-    <div className="card w-96 bg-base-100 shadow-xl image-full">
-      <figure>
-        <img src={temp} alt="Shoes" />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+    {
+      company: "Zennya",
+      position: "Backend Developer",
+      project: "Zennya API",
+      info: "Developed APIs for mobile and web applications.Researched, documented and implemented proof of conceptsto be used as new features. Provided documentation for new api endpoints and new featuresimplemented. Participated in meetings and logged daily progress in Jira",
+    },
+
+    {
+      company: "GreatFeat Services Inc.",
+      position: "Software Developer",
+      project: "Online Gaming App",
+      info: "Developed web service to manage new game platforms Participated in team meetings, requirement gathering for new features Modified fields and structure of database for optimization.",
+    },
+  ];
+
+  const workCardMap = workData.map((data) => {
+    return (
+      <div className="card lg:card-side bg-base-100 shadow-xl text-primary">
+        <figure>
+          <img src={temp} alt="Album" className="max-w-[400px] max-h-[400px]" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title text-2xl lg:text-3xl xl:text-4xl">
+            {data.company}
+          </h2>
+          <p className="text-sm lg:text-base xl:text-lg">
+            {`POSITION: ${data.position}`} <br />
+            {`PROJECT: ${data.project}`} <br />
+            {`INFO: ${data.info}`} <br />
+          </p>
         </div>
       </div>
-    </div>
-  );
-
-  const workCardMap = [workCard, workCard, workCard, workCard];
+    );
+  });
 
   return (
-    <div className="section">
-      <div className="hero min-h-screen bg-primary text-secondary">
-        <div className="hero-content max-w-full">
+    <div className=" min-h-screen bg-primary text-secondary">
+      <div className="hero-content  min-w-screen max-w-full">
+        <div className="carousel-container min-h-screen">
           <div>
-            <h1 className="text-5xl font-bold ">Work Experience!</h1>
-            <div className="flex flex-wrap gap-4">{workCardMap}</div>
+            <h1
+              className={` font-extrabold  pb-10
+              ${isDesktop ? "header-desktop pt-40" : "header-mobile pt-20"}
+              `}
+            >
+              Professional Experience
+            </h1>
+            <Carousel
+              showThumbs={true}
+              showStatus={false}
+              infiniteLoop={true}
+              className="carousel-full-screen"
+            >
+              {workCardMap.map((card, index) => (
+                <div
+                  key={index}
+                  className="carousel-slide-content flex justify-center items-center"
+                >
+                  {card}
+                </div>
+              ))}
+            </Carousel>
           </div>
         </div>
       </div>
